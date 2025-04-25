86307e97067449e76e8d8130b3e6ba47cdc0291c
Haziel Magallanes <hazielescuela1@gmail.com>
2025-04-25 16:24:48 -0300

    Added InputField and ResponsiveComponent interface

---
+++ src/components/session/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/index.css";
+import InputField from "../UI/InputField";
+
+const Login: React.FC = () => {
+    return (
+        <div>Hello world!</div>
+    );
+};
+
+export default Login;
