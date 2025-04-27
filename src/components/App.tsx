5bf9decfed8e63e57af47253965b94f221694add
DeepZar <deepzar77@gmail.com>
2025-04-27 17:31:37 -0300

    Action Button
    Añadido el botón con texto editable

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 
 import AppWindow from './UI/AppWindow'
+import ActionButton from './UI/ActionButton'
 
 function App() {
  return (
     <div className="App">
       <AppWindow width={350} height={620}/>
+      <ActionButton height={60} text="Iniciar Sesión"/>
     </div>
+
  )
 }
 
