bb3119f90740255a71eb34b88012e417dca8f49b
DeepZar <deepzar77@gmail.com>
2025-04-28 15:02:54 -0300

    NavBar and Box Shadows

---
+++ src/components/UI/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../styles/index.css";
+import notification_icon from "../../assets/navBar/bxs-bell-ring.svg";
+import about_us_icon from "../../assets/navBar/bxs-info-circle.svg";
+import phone_icon from "../../assets/navBar/bxs-phone.svg";
+import unitec_text from "../../assets/navBar/unitec_Texto_Color_SVG 2.svg";
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="nav-bar">
+            <div className="logo-section">
+                <img src={unitec_text} alt="Unitec Text" className="unitec-text"/>
+            </div>
+            <div className="icons-section">
+                <img src={about_us_icon} alt="About Us" className="about-us-icon"/>
+                <img src={phone_icon} alt="Notification" className="phone-icon" />
+                <img src={notification_icon} alt="Notification" className="notification-icon" style={{display: "none"}}/>
+                <div className="profile-pic"></div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
