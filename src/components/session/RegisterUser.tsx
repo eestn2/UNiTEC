a15d8b4de52f1f0a6264fa80042974018ad79759
DeepZar <deepzar77@gmail.com>
2025-05-06 20:50:52 -0300

    RegisterUser window in process
    Update made to work on other device properly and to save the window progress

--- src/components/session/RegisterUser.tsx
+++ src/components/session/RegisterUser.tsx
@@ -6,33 +6,68 @@ import NavBar from '../UI/NavBar'
 import AppWindow from '../UI/AppWindow'
 import ActionButton from '../UI/ActionButton'
 import Login from "./Login";
+import Logo from "../UI/Logo";
+import TranslateFigmaCoords from "../../global/function/TranslateFigmaCoords";
+import InputField from "../UI/InputField";
+import TextBox from "../UI/textBox";
 
 function RegisterUser() {
  return (
-    <div className="register-user">
-      <div className="side-sections">
+  <>
+    <Logo className="watermark"></Logo>
+    <AppWindow width={635} height={580} style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      rowGap: TranslateFigmaCoords.translateFigmaY(20),
+      padding: TranslateFigmaCoords.translateFigmaY(20),
+      flexDirection: "column",
+      position: "absolute", 
+      top: "50%", left: "50%", translate: "-50% -50%"
+    }}>
+        <span className="top-section title" 
+        style={{
+          height: TranslateFigmaCoords.translateFigmaY(80),
+          width: "100%",
+        }}> Registro de la Empresa</span>
 
-      </div>
-      <AppWindow height={635} width={620}>
-        <div className="name-section">
-          <h1>Registro de Usuario</h1>
-        </div>
-        <div className="line" style={{
-          height: "4px",
-          right: (342 / 1280) * innerWidth}}></div>
-        <div className="horizontal-section">
-          <div className="data-section">
+        <div className="horizontal-display">
+            <div className="left-section" style={{
+            display: "flex",
+            flexDirection: "column",
+            paddingRight: `${TranslateFigmaCoords.translateFigmaX(20)}px`,
+            height: "100%",
+            width: "50%",
+            gap: "15px", // Added gap between InputFields
+            }}>
+            <InputField name="name-enterprise" type="text" placeholder="Nombre de la Empresa" width={305} height={55}/>
+            <InputField name="mail-enterprise" type="text" placeholder="Correo Electrónico" width={305} height={55}/>
+            <InputField name="password-enterprise" type="text" placeholder="Contraseña" width={305} height={55}/>
+            <InputField name="password-confirm-enterprise" type="text" placeholder="Confirmar Contraseña" width={305} height={55}/>
+            <InputField name="website-enterprise" type="text" placeholder="Enlace a su Página Web (Opcional)" width={305} height={55}/>
+            <TextBox name="website-enterprise" placeholder="Descripción" width={305} height={110}/>
+            </div>
 
+          <div className="right-section" 
+          style={{
+            height: "100%",
+            width: "50%",}}>
+            <div className="half-component-top" 
+            style={{
+              height: "40%",
+              width: "100%",
+            }}></div>
+            <div className="half-component-bottom" 
+            style={{
+              height: "60%",
+              width: "100%",
+            }}></div>
           </div>
-          <div className="line" style={{width: "4px", height: "90%"}}></div>
-          <div className="data-section">
-
-          </div>
         </div>
-      </AppWindow>
-      <div className="side-sections"></div>
-    </div>
-
+        
+    </AppWindow>
+  </>
+    
  )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import './styles/index.css'
 import FeedBox from './components/feed/FeedBox'
+import RegisterUser from './components/session/RegisterUser'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FeedBox />
+    <RegisterUser/>
   </StrictMode>,
 )
