ae8f61bf10c21f03f602dc90b9c809024df40313
Fede <fedeprooxd910@gmail.com>
2025-05-09 11:06:39 -0300

    user-info created

---
+++ src/php/requests/user/user-info.php
@@ -0,0 +1,23 @@
+<?php
+//A partir de la ID de usuario, devolver nombre y foto de perfil
+require_once "../cors-policy.php";
+require_once __DIR__ . '/../../logic/connect_to_database.php';
+require_once __DIR__ . '../function/return_response.php';
+if ($_SERVER["REQUEST_METHOD"] !== "GET") return_response("failed", "Metodo no permitido.", null);
+$data = json_decode(file_get_contents("php://input"));
+if (!isset($data->id)) return_response("failed", "Ocurrio un error, intente de nuevo.", null);
+
+$id = $data->id;
+$stmt = $connection->prepare("SELECT * FROM users WHERE id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+return_response("success", "Datos del usuario " . $user . " devueltos correctamente.", ["user" => [
+    "name" => $user["name"],
+    "profile_picture" => $user["profile_picture"]
+]]);
+$stmt->close();
+?>
