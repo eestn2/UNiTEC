5c3df60fdcf6425cf1be8344200d31abdf9b937a
Fede <fedeprooxd910@gmail.com>
2025-05-15 10:08:00 -0300

    accept-application & reject-application created

---
+++ src/php/requests/enterprise/accept-application.php
@@ -0,0 +1,58 @@
+<?php
+require_once __DIR__ . "/../cors-policy.php";
+require_once __DIR__ . '/../../logic/connection.php';
+require_once __DIR__ . '/../function/return_response.php';
+
+if ($_SERVER["REQUEST_METHOD"] !== "PUT") return_response("failed", "Metodo no permitido.", null);
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->creator_id) || !isset($data->user_id) || !isset($data->application_id)) return_response("failed", "Faltan datos obligatorios.", null);
+
+$creator_id = intval($data->creator_id);
+$user_id = intval($data->user_id);
+$application_id = intval($data->application_id);
+$accepted_status_id = 1; //Acepted
+
+try{
+    $connection->beginTransaction();
+    // Verificar que el creator_id es una empresa
+    $stmt = $connection->prepare("SELECT user_type_id FROM users WHERE id = :id");
+    $stmt->bindParam(':id', $creator_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$user || intval($user['user_type_id']) !== 1){
+        return_response("failed", "El usuario no tiene permisos para aceptar postulantes.", null);
+    }
+
+    // Verificar que esa oferta fue creada por esa empresa
+    $stmt = $connection->prepare("SELECT * FROM applications WHERE id = :application_id AND creator_id = :creator_id");
+    $stmt->bindParam(':application_id', $application_id, PDO::PARAM_INT);
+    $stmt->bindParam(':creator_id', $creator_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $application = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$application) {
+        return_response("failed", "No se encontró la oferta de trabajo o no pertenece al usuario.", null);
+    }
+
+    // Actualizar estado del postulante
+    $stmt = $connection->prepare("UPDATE applicants SET status_id = :status_id WHERE user_id = :user_id AND application_id = :application_id");
+    $stmt->bindParam(':status_id', $accepted_status_id, PDO::PARAM_INT);
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindParam(':application_id', $application_id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $connection->commit();
+        return_response("success", "Postulante aceptado con éxito.", null);
+    } else {
+        $connection->rollBack();
+        return_response("failed", "No se pudo aceptar al postulante o ya fue aceptado anteriormente.", null);
+    }
+}catch (PDOException $e){
+    if ($connection->inTransaction()) {
+        $connection->rollBack();
+    }
+    return_response("failed", "Error en el servidor:" . $e->getMessage(), null);
+}
+
+?>
